a2b5c86f16893a1ffbaebda3d6e7524d7d712020
iandol <iandol@seneca.org>
2024-05-30 13:04:18 +0800

    doc

--- filters/enstyle-list.lua
+++ filters/enstyle-list.lua
@@ -1,5 +1,9 @@
--- the purpose of this filter is to take ordered and bulleted lists and add 
--- custom-styles to the first and subsequent items
+--[[
+	The purpose of this filter is to take ordered and bulleted lists and add 
+	custom-styles to the first and subsequent items…
+	Version:   1.00
+	Copyright: (c) 2024 Ian Max Andolina License=MIT, see LICENSE for details
+]]
 
 local olfirst	= "Numbered 1"
 local olrest	= "Numbered"
